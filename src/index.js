939dfb8fc6eabc0d851d4db56456d6f82d281265
Sungchunn <99805001+Sungchunn@users.noreply.github.com>
2025-11-07 17:26:53 +0700

    feat: Configure for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import AnalysisPage from './pages/AnalysisPage';
 import PDFPage from './pages/PDFPage';
@@ -8,12 +8,12 @@ import PDFPage from './pages/PDFPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/analysis" element={<AnalysisPage />} />
         <Route path="/pdf" element={<PDFPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
